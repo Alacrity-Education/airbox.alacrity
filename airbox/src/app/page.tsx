fa00cbf71b8914480ea3555f11802c5fb6a47986
Ionescu Andrei <andreiionescu750@gmail.com>
2025-02-12 05:29:33 +0200

    setup

---
+++ airbox/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import React from "react";
+
+
+export default function Home() {
+  return (
+    <div className="flex align-center justify-center">
+      <button className="btn">Default</button>
+    </div>
+  );
+}
